4b83a8c2ca615dc37e5273d99259f5cd19e03bb4
tindal-966 <qinghuang1995@gmail.com>
2022-08-29 02:57:02 +0800

    refactor: add 59.Spiral Matrix II

--- src/main/java/org/example/spiralMatrixIi/Solution.java
+++ src/main/java/org/example/spiralMatrixIi/Solution.java
@@ -23,60 +23,54 @@
  ******************************************************************************************************/
 package org.example.spiralMatrixIi;
 
+/**
+ * Assume matrix is:
+ *  1,  2,  3,  4,  5,
+ * 16, 17, 18, 19,  6,
+ * 15, 24, 25, 20,  7,
+ * 14, 23, 22, 21,  8,
+ * 13, 12, 11, 10,  9
+ *
+ * Notes:
+ * 1. if one circle is a layer, it has *(n+1)/2* layer, num 25 is a special layer
+ * 2. if every direction in circle we only handle *n - layer * 2 - 1*(first layer is 0) nums, the handle times every direction is the same
+ *      layer 0: 4. first 1,2,3,4; second 5,6,7,8; third 9,10,11,12
+ *      layer 1: 2
+ *      layer 2: 0, special handle
+ * 3. every direction first number's coordination(also is corner in circle)
+ *      top-left [layer, layer]
+ *      top-right [layer, n - 1 - layer]
+ *      bottom-right [n - 1 - layer, n - 1 - layer]
+ *      bottom-left [n - 1 - layer, layer]
+ */
 class Solution {
     public int[][] generateMatrix(int n) {
-        int[][] a = new int[n][n];
+        int[][] result = new int[n][n];
 
-        int direction = 0; // 0: right; 1: down; 2: left; 3: up;
-        int[] directionWithStep = {n, n - 1, n - 1, n - 2};
-        int currentStep = 0;
-        int size = n * n;
-        int row = 0, column = 0;
-        for (int i = 1; i <= size; i++) {
-            if (currentStep == directionWithStep[direction % 4] || directionWithStep[direction % 4] < 0) {
-                if ((direction / 4) != ((direction + 1) / 4)) { // second time to right, re-calculate the step
-                    calStep(directionWithStep);
-                }
-                direction += 1;
-                if (direction % 4 == 0) {
-                    column++;
-                    row++; // because first row--, so rollback
-                } else if (direction % 4 == 1) {
-                    row++;
-                    column--;
-                } else if (direction % 4 == 2) {
-                    column--;
-                    row--;
-                } else {
-                    row--;
-                    column++;
-                }
-                currentStep = 0;
+        int currentValue = 1, totalLayer = (n + 1) / 2;
+        for (int layer = 0; layer < totalLayer; layer++) {
+            int count = n - layer * 2 - 1; // handle times in every direction
+
+            if (count == 0) { // the last layer which has odd layer, special handle
+                result[layer][layer] = currentValue;
+                break;
             }
 
-            a[row][column] = i;
-
-            // how move when direction
-            if (direction % 4 == 0) {
-                column++;
-            } else if (direction % 4 == 1) {
-                row++;
-            } else if (direction % 4 == 2) {
-                column--;
-            } else {
-                row--;
+            for (int j = 0; j < count; j++) {
+                result[layer][layer + j] = currentValue++;
             }
-
-            currentStep++;
+            for (int j = 0; j < count; j++) {
+                result[layer + j][n - 1 - layer] = currentValue++;
+            }
+            for (int j = 0; j < count; j++) {
+                result[n - 1 - layer][n - 1 - layer - j] = currentValue++;
+            }
+            for (int j = 0; j < count; j++) {
+                result[n - 1 - layer - j][layer] = currentValue++;
+            }
         }
 
-        return a;
-    }
-
-    public void calStep(int[] a) {
-        for (int i = 0; i < a.length; i++) {
-            a[i] -= 2;
-        }
+        return result;
     }
 }
 
