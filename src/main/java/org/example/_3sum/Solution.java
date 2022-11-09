7cf979d49eec4e847b34c8e286ca085ac1589e0f
tindoc <qinghuang1995@gmail.com>
2022-11-09 16:53:20 +0800

    refactor: 15. 3Sum
    timeout

--- src/main/java/org/example/_3sum/Solution.java
+++ src/main/java/org/example/_3sum/Solution.java
@@ -5,18 +5,36 @@ import java.util.stream.Collectors;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<String> set = new HashSet<>();
-        List<List<Integer>> result = new ArrayList<>();
+        Map<Integer, List<Set<Integer>>> map = new HashMap<>(); // Map<sum, List<IndexTuple>>
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length && j != i; j++) { // i != j
+                int sum = nums[i] + nums[j];
 
-        int length = nums.length;
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length && j != i; j++) {
-                for (int k = 0; k < length && k != i && k != j; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        if (!set.contains(getSetKey(nums[i], nums[j], nums[k]))) {
-                            result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                            set.add(getSetKey(nums[i], nums[j], nums[k]));
-                        }
+                Set<Integer> set = new HashSet<>();
+                set.add(i);
+                set.add(j);
+
+                List<Set<Integer>> orDefault = map.getOrDefault(sum, new ArrayList<>());
+                orDefault.add(set);
+
+                map.put(sum, orDefault);
+            }
+        }
+
+        Set<String> resultSet = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(-nums[i])) {
+                List<Set<Integer>> lists = map.get(-nums[i]);
+                for (Set<Integer> set : lists) {
+                    String setKey = getSetKey(nums[i], nums, set.toArray(new Integer[0]));
+                    if (!set.contains(i) && !resultSet.contains(setKey)) { // k != i, k != j
+                        List<Integer> tmp = new ArrayList<>();
+                        tmp.add(nums[i]);
+                        set.forEach(e -> tmp.add(nums[e]));
+
+                        result.add(tmp);
+                        resultSet.add(setKey);
                     }
                 }
             }
@@ -25,9 +43,9 @@ class Solution {
         return result;
     }
 
-    private String getSetKey(int i1, int i2, int i3) {
-        int[] ints = {i1, i2, i3};
-        return Arrays.stream(ints)
+    private String getSetKey(int i1, int[] nums, Integer[] indexList) {
+        int[] newInts = new int[]{i1, nums[indexList[0]], nums[indexList[1]]};
+        return Arrays.stream(newInts)
                 .sorted()
                 .mapToObj(String::valueOf)
                 .collect(Collectors.joining(" "));
